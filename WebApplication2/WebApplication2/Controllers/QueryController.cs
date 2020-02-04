a71359d0cc127cb406cd31e01d861fccd6d474ae
Joshua <joh71408@gmail.com>
2020-02-04 19:15:22 +0800

    2020/02/04-Query

---
+++ WebApplication2/WebApplication2/Controllers/QueryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class QueryController : Controller
+    {
+        // GET: OrderQuery
+        public ActionResult OrderQuery()
+        {
+            return View();
+        }
+
+        public ActionResult InStockQuery()
+        {
+            return View();
+        }
+
+        public ActionResult InventoryQuery()
+        {
+            return View();
+        }
+
+        public ActionResult Query()
+        {
+            return View();
+        }
+    }
+}
