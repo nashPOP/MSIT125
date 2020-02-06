20c6b9ab1a99caf1453c1a5b642426692a756062
howardchang823 <howardchang823@gmail.com>
2020-02-06 19:21:32 +0800

    Howard_first_update

---
+++ WebApplication2/WebApplication2/Controllers/EnterStockController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class EnterStockController : Controller
+    {
+        // GET: EnterStock
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EnterStock()
+        {
+            return View();
+        }
+    }
+}
